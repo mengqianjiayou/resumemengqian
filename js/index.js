74f2c5191620c2ba7ea23df4552b3a7ea5676617
mengqianjiayou <798071102@qq.com>
2017-05-20 20:09:56 +0800

    第一屏添加背景色

--- js/index.js
+++ js/index.js
@@ -3,33 +3,6 @@ var container = document.getElementsByClassName('container')[0],
     square = wrapper.getElementsByClassName('square')[0],
     oUl = square.getElementsByTagName('ul'),
     winHeight = document.documentElement.clientHeight;
-/*function creatLi() {
-    var winw = document.documentElement.clientWidth;
-    for (var i = 0; i < oUl.length; i++) {
-        var curUl = oUl[i],
-            ulHeight = 0;
-        while (ulHeight < winHeight) {
-            var curLi = document.createElement('li');
-            var random = (Math.round(Math.random() * 100 + 50)) / 100;
-            curLi.style.height = random + 'rem';
-            curUl.appendChild(curLi);
-            ulHeight += random * 100 * (winw / 750);
-        }
-    }
-}
-creatLi();*/
-/*function fadeIn() {
-    var olis = square.getElementsByTagName('li');
-    for(var i= 0;i<olis.length;i++){
-        var random = (Math.random()*4+7.3)+'s';
-//            var ran2 = Math.round(Math.random()*400-200)+'px';
-//            olis[i].style.webkitTransform = 'translateZ('+ran2+')';
-//            olis[i].style.transform = 'translateZ('+ran2+')';
-        olis[i].style.webkitAnimationDelay = random;
-        olis[i].style.animationDelay = random;
-    }
-}
-fadeIn();*/
 var slides = container.getElementsByClassName('slide');
 var slidesAry = [].slice.call(slides,0);
 var index = 0;
@@ -54,34 +27,36 @@ function autoMove() {
     }
     setSlide();
 }
-var timer = setInterval(autoMove,18000);
 var oBody = document.getElementById('body');
-oBody.onTouchStart= function (e) {
-    e.preventDefault();
-    console.log(1);
-    var startX = e.originalEvent.changedTouches[0].pageX;
-    var startY = e.originalEvent.changedTouches[0].pageY;
-};
-oBody.onTouchMove=function (e) {
-    e.preventDefault();
-    var moveEndX =e.originalEvent.changedTouches[0].pageX,
-        moveEndY = e.originalEvent.changedTouches[0].pageY,
-        X = moveEndX -startX,
-        Y = moveEndY - startY;
-    if(Y>0){
-        index ++;
-        if(index == slidesAry.length){
+oBody.timer = setInterval(autoMove,18000);
+oBody.addEventListener('touchstart',function(e){
+     e = e||event;
+    window.clearInterval(oBody.timer);
+    oBody.startY = e.touches[0].pageY;
+    console.log(oBody.startY);
+});
+oBody.addEventListener('touchmove',function(e){
+     e  = e||event;
+    var endY = e.touches[0].pageY;
+    oBody.changeY = endY-oBody.startY;
+});
+oBody.addEventListener('touchend',function(e){
+    e  = e||event;
+    if(oBody.changeY<0){
+        index++;
+        if(index === slidesAry.length){
             index = 0;
         }
-    }
-    if(Y<0){
+    }else{
         index--;
-        if(index == -1){
+        if(index === -1){
             index = slidesAry.length-1;
         }
     }
     setSlide();
-};
+    oBody.timer = setInterval(autoMove,18000);
+});
+
 var pointer =document.getElementById('pointer');
 pointer.onclick = function () {
     index++;
@@ -89,5 +64,5 @@ pointer.onclick = function () {
         index = 0;
     }
     setSlide();
-}
+};
 
